775d50cc8aaec7e91d5e84fb056c2ca765fac7ad
ArmanWali <armanwali2019@gmail.com>
2025-02-13 23:45:38 +0500

    create new react app for Fun Facts about React

---
+++ ReactFacts/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+	return (
+		<>
+			<img src={reactLogo} className="App-logo" alt="logo" />
+
+			<ul>
+				<h1>Fun facts about React</h1>
+				<li>Was first realesed in 2013</li>
+				<li>Was originally created by Jordan Walke</li>
+				<li>Has well over 100k stars on GitHub</li>
+				<li>Is maintained by Meta</li>
+				<li>Powers thousands of enterprise apps, including mobile apps</li>
+			</ul>
+			
+		</>
+	)
+}
+
+export default App
