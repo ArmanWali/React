884f775574ce954b5ead7ded81854e4d2f6ee012
ArmanWali <armanwali2019@gmail.com>
2025-02-16 13:29:12 +0500

    Adding some more places

--- MyTravelJournal/src/data.js
+++ MyTravelJournal/src/data.js
@@ -35,4 +35,40 @@ export default [
         dates: "01 Oct, 2021 - 18 Nov, 2021",
         text: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
     },
-]
+    {
+        id: 4,
+        img: {
+            src: "https://upload.wikimedia.org/wikipedia/commons/e/e3/Kheops-Pyramid.jpg",
+            alt: "Pyramids of Giza"
+        },
+        title: "Pyramids of Giza",
+        country: "Egypt",
+        googleMapsLink: "https://maps.app.goo.gl/Yc4Fq3VZpLsHfT8m9",
+        dates: "5 Mar, 2022 - 19 Mar, 2022",
+        text: "The Pyramids of Giza are one of the most iconic wonders of the ancient world, built over 4,500 years ago."
+    },
+    {
+        id: 5,
+        img: {
+            src: "https://cdn.britannica.com/36/162636-050-932C5D49/Colosseum-Rome-Italy.jpg",
+            alt: "Colosseum"
+        },
+        title: "Colosseum",
+        country: "Italy",
+        googleMapsLink: "https://maps.app.goo.gl/AKBpRd1PKGxC6dqp7",
+        dates: "10 Jul, 2022 - 24 Jul, 2022",
+        text: "The Colosseum in Rome is an ancient amphitheater used for gladiator battles and public spectacles."
+    },
+    {
+        id: 6,
+        img: {
+            src: "https://images.pexels.com/photos/9977483/pexels-photo-9977483.jpeg",
+            alt: "Christ the Redeemer"
+        },
+        title: "Christ the Redeemer",
+        country: "Brazil",
+        googleMapsLink: "https://maps.app.goo.gl/hqQzpmXy9RY8z7mn6",
+        dates: "15 Sep, 2022 - 30 Sep, 2022",
+        text: "Christ the Redeemer is a colossal statue of Jesus Christ in Rio de Janeiro, Brazil, offering breathtaking city views."
+    }
+];
