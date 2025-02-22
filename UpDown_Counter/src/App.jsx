102ff037eaf6248c2deee035e1988a3237fd2cef
ArmanWali <armanwali2019@gmail.com>
2025-02-22 21:01:09 +0500

    creating buttons for it

--- UpDown_Counter/src/App.jsx
+++ UpDown_Counter/src/App.jsx
@@ -3,9 +3,14 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      
-    </>
+    <main className='container'>
+      <h1>Up-Down Counter</h1>
+      <div className="counter">
+        <h2>0</h2>
+        <button className="btn">-</button>
+        <button className="btn">+</button>
+      </div>
+    </main>
   )
 }
 
